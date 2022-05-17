318e71d2ba7d947e9090e0e733cbd71af8253fc4
stolpem <50697783+stolpem@users.noreply.github.com>
2022-05-17 13:58:57 +0000

    Updated names and ports

--- starter_files/benchmark.sh
+++ starter_files/benchmark.sh
@@ -70,4 +70,4 @@
 # Total:        114  160  28.0    172     199#
 
 
- ab -n 10 -v 4 -p data.json -T 'application/json' -H 'Authorization: Bearer REPLACE_WITH_KEY' http://REPLACE_WITH_API_URL/score
+ ab -n 10 -v 4 -p data.json -T 'application/json' -H 'Authorization: Bearer rQY6sbXEaVtSNuSza0TVQAbkksa5t61r' http://ee9a18e1-e671-4b97-b626-8142d6dc7f40.southcentralus.azurecontainer.io/score
--- starter_files/endpoint.py
+++ starter_files/endpoint.py
@@ -3,59 +3,66 @@ import json
 
 # URL for the web service, should be similar to:
 # 'http://8530a665-66f3-49c8-a953-b82a2d312917.eastus.azurecontainer.io/score'
-scoring_uri = ''
+scoring_uri = 'http://ee9a18e1-e671-4b97-b626-8142d6dc7f40.southcentralus.azurecontainer.io/score'
 # If the service is authenticated, set the key or token
-key = ''
+key = 'rQY6sbXEaVtSNuSza0TVQAbkksa5t61r'
 
 # Two sets of data to score, so we get two results back
-data = {"data":
+data = {
+    "Inputs": {
+        "data":
         [
-          {
-            "age": 17,
-            "campaign": 1,
-            "cons.conf.idx": -46.2,
-            "cons.price.idx": 92.893,
-            "contact": "cellular",
-            "day_of_week": "mon",
-            "default": "no",
-            "duration": 971,
-            "education": "university.degree",
-            "emp.var.rate": -1.8,
-            "euribor3m": 1.299,
-            "housing": "yes",
-            "job": "blue-collar",
-            "loan": "yes",
-            "marital": "married",
-            "month": "may",
-            "nr.employed": 5099.1,
-            "pdays": 999,
-            "poutcome": "failure",
-            "previous": 1
-          },
-          {
-            "age": 87,
-            "campaign": 1,
-            "cons.conf.idx": -46.2,
-            "cons.price.idx": 92.893,
-            "contact": "cellular",
-            "day_of_week": "mon",
-            "default": "no",
-            "duration": 471,
-            "education": "university.degree",
-            "emp.var.rate": -1.8,
-            "euribor3m": 1.299,
-            "housing": "yes",
-            "job": "blue-collar",
-            "loan": "yes",
-            "marital": "married",
-            "month": "may",
-            "nr.employed": 5099.1,
-            "pdays": 999,
-            "poutcome": "failure",
-            "previous": 1
-          },
-      ]
+            {
+                "age": "17",
+                "job": "blue-collar",
+                "marital": "married",
+                "education": "university.degree",
+                "default": "no",
+                "housing": "yes",
+                "loan": "yes",
+                "contact": "cellular",
+                "month": "may",
+                "day_of_week": "mon",
+                "duration": "971",
+                "campaign": "1",
+                "pdays": "999",
+                "previous": "1",
+                "poutcome": "failure",
+                "emp.var.rate": "-1.8",
+                "cons.price.idx": "92.893",
+                "cons.conf.idx": "-46.2",
+                "euribor3m": "1.299",
+                "nr.employed": "5099.1"
+            },
+            {
+                "age": "87",
+                "job": "blue-collar",
+                "marital": "married",
+                "education": "university.degree",
+                "default": "no",
+                "housing": "yes",
+                "loan": "yes",
+                "contact": "cellular",
+                "month": "may",
+                "day_of_week": "mon",
+                "duration": "471",
+                "campaign": "1",
+                "pdays": "999",
+                "previous": "1",
+                "poutcome": "failure",
+                "emp.var.rate": "-1.8",
+                "cons.price.idx": "92.893",
+                "cons.conf.idx": "-46.2",
+                "euribor3m": "1.299",
+                "nr.employed": "5099.1"
+            },
+        ]
+    },
+    "GlobalParameters": {
+        "method": "predict"
     }
+}
+
 # Convert to JSON string
 input_data = json.dumps(data)
 with open("data.json", "w") as _f:
@@ -69,5 +76,3 @@ headers['Authorization'] = f'Bearer {key}'
 # Make the request and display the response
 resp = requests.post(scoring_uri, input_data, headers=headers)
 print(resp.json())
-
-
